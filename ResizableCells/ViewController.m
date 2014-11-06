662a759b3d1263f2538c46d0a9866e5364d18e13
Nordin <xxxxxx@gmail.com>
2014-11-06 15:45:24 +0100

    This is a bit working.

--- ResizableCells/ViewController.m
+++ ResizableCells/ViewController.m
@@ -21,8 +21,8 @@ static NSString *ChatCellIdentifier = @"chatCell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.tableView.estimatedRowHeight = 68.0;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    //self.tableView.estimatedRowHeight = 68.0;
+    //self.tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,11 +49,15 @@ static NSString *ChatCellIdentifier = @"chatCell";
 {
     if ([cell isKindOfClass:[DVChatCell class]])
     {
-        NSLog(@"Cell is of type DVChatCell");
+        //NSLog(@"Cell is of type DVChatCell");
         DVChatCell *textCell = (DVChatCell *)cell;
         //textCell.labelUser.text = [NSString stringWithFormat:@"Line %ld",(long)indexPath.row+1];
         //textCell.labelUser.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1];
+        
+        //textCell.labelMessage.numberOfLines = 0;
+        //textCell.labelMessage.lineBreakMode = NSLineBreakByWordWrapping;
         textCell.labelMessage.text = @"Note that we set the preferred font for the two labels each time we configure the cell in case the user has changed the text size.";
+        [textCell.labelMessage sizeToFit];
         //textCell.labelMessage.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
     } else {
         NSLog(@"Cell is NOT of type DVChatCell");
@@ -62,8 +66,7 @@ static NSString *ChatCellIdentifier = @"chatCell";
 
 - (DVChatCell *)prototypeCell
 {
-    if (!_prototypeCell)
-    {
+    if (!_prototypeCell) {
         _prototypeCell = [self.tableView dequeueReusableCellWithIdentifier:ChatCellIdentifier];
     }
     return _prototypeCell;
@@ -72,11 +75,27 @@ static NSString *ChatCellIdentifier = @"chatCell";
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if (!_prototypeCell)
+        _prototypeCell = [self.tableView dequeueReusableCellWithIdentifier:ChatCellIdentifier];
+    /*
     [self configureCell:self.prototypeCell forRowAtIndexPath:indexPath];
+
+    [self.prototypeCell setNeedsLayout];
     [self.prototypeCell layoutIfNeeded];
     
     CGSize size = [self.prototypeCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
-    return size.height+1;
+    */
+    [self configureCell:self.prototypeCell forRowAtIndexPath:indexPath];
+    [self.prototypeCell layoutIfNeeded];
+    [self.prototypeCell.labelMessage sizeToFit];
+    float height = self.prototypeCell.labelMessage.frame.size.height;
+    
+    NSLog(@"Cell height: %f", height);
+    return height;
+
+    //NSLog(@"Cell height: %f", size.height);
+    //return size.height;
+    //return 120.0f;
 }
 
 /*
